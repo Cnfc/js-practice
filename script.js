c0ef55dcdca07929f0e3352b35c054c3787957da
Станислав <TMWCnf@gmail.com>
2017-09-10 19:47:11 +0300

    start second

--- script.js
+++ script.js
@@ -9,11 +9,17 @@
 //     }
 // }
 
-function fun1 () {
-  var radi = document.getElementsByName('r1');
-  for (var i = 0; i< radi.length; i++) {
-    if(radi[i].checked) {
-      alert( "Init " + i + " element");
-    }
-  }
+// function fun1 () {
+//   var radi = document.getElementsByName('r1');
+//   for (var i = 0; i< radi.length; i++) {
+//     if(radi[i].checked) {
+//       alert( "Init " + i + " element");
+//     }
+//   }
+// }
+
+function func1() {
+  var sel = document.getElementById('mySelect').selectedIndex;
+  var options = document.getElementById('MySelect').options;
+  alert("Init option " +options[sel].text);
 }
