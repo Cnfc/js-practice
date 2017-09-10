a8c7b8705d60a9982d758086b247232d75400cf5
Станислав <TMWCnf@gmail.com>
2017-09-10 19:23:44 +0300

    first done

---
+++ script.js
@@ -0,0 +1,11 @@
+function func1() {
+    var chbox;
+    chbox = document.getElementById('one');
+
+    if (chbox.checked) {
+      alert('Init');
+    } else {
+      alert('Not init');
+    }
+}
+
