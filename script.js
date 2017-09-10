27ac724caef848157e55592016473df74ae6a03f
Станислав <TMWCnf@gmail.com>
2017-09-10 19:34:08 +0300

    first+ done

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function func1() {
-    var chbox;
-    chbox = document.getElementById('one');
+// function func1() {
+//     var chbox;
+//     chbox = document.getElementById('one');
 
-    if (chbox.checked) {
-      alert('Init');
-    } else {
-      alert('Not init');
+//     if (chbox.checked) {
+//       alert('Init');
+//     } else {
+//       alert('Not init');
+//     }
+// }
+
+function fun1 () {
+  var radi = document.getElementsByName('r1');
+  for (var i = 0; i< radi.length; i++) {
+    if(radi[i].checked) {
+      alert( "Init " + i + " element");
     }
+  }
 }
-
