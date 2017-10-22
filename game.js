21c70fedbb8e719672120867178ec41874979235
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-22 17:37:39 +0300

    add while problem set

---
+++ game.js
@@ -0,0 +1,32 @@
+//create secrent number
+var a = -10;
+while (a <= 19) {
+    console.log(a);
+    a++;
+}
+
+console.log("NEW ROUND");
+var b = 10;
+while (b <= 40) {
+    console.log(b);
+    b += 2;
+}
+
+console.log("NEW ROUND");
+var c = 300;
+while (c <= 333) {
+  if (c % 2 !== 0) {
+    console.log(c);
+  }
+    c++;
+}
+
+console.log("NEW ROUND!!!!!!!!!");
+
+var d = 5;
+while( d <= 50 ) {
+  if ((d % 3 === 0) && (d % 5 === 0)) {
+    console.log(d);
+  }
+  d++;
+}
