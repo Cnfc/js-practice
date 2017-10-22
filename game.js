1c42852a21683f420edd9df78370518d643010a5
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-22 19:23:17 +0300

    add isEven

--- game.js
+++ game.js
@@ -1,32 +1,60 @@
-//create secrent number
-var a = -10;
-while (a <= 19) {
-    console.log(a);
-    a++;
-}
+// //create secrent number
+// var a = -10;
+// while (a <= 19) {
+//     console.log(a);
+//     a++;
+// }
 
-console.log("NEW ROUND");
-var b = 10;
-while (b <= 40) {
-    console.log(b);
-    b += 2;
-}
+// console.log("NEW ROUND");
+// var b = 10;
+// while (b <= 40) {
+//     console.log(b);
+//     b += 2;
+// }
 
-console.log("NEW ROUND");
-var c = 300;
-while (c <= 333) {
-  if (c % 2 !== 0) {
-    console.log(c);
-  }
-    c++;
-}
+// console.log("NEW ROUND");
+// var c = 300;
+// while (c <= 333) {
+//   if (c % 2 !== 0) {
+//     console.log(c);
+//   }
+//     c++;
+// }
 
-console.log("NEW ROUND!!!!!!!!!");
+// console.log("NEW ROUND!!!!!!!!!");
 
-var d = 5;
-while( d <= 50 ) {
-  if ((d % 3 === 0) && (d % 5 === 0)) {
-    console.log(d);
-  }
-  d++;
-}
+// var d = 5;
+// while( d <= 50 ) {
+//   if ((d % 3 === 0) && (d % 5 === 0)) {
+//     console.log(d);
+//   }
+//   d++;
+// }
+
+
+
+
+// function isEven(x) {
+//   if (x % 2 === 0 ){
+//     return true;
+//   } else {
+//   return false;
+//   }
+// }
+// isEven();
+
+// console.log("NEW ROUND");
+
+// function isEven(num) {
+//   return num % 2 ===0;
+// }
+
+
+// function factorial(num) {
+//   var result = 1;
+//   for(var i = 1; i <=num; i++) {
+//     result *= i;
+//   }
+//   return result
+// }
+
