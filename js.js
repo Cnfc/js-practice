66f16d56b606aea6dca76f75bd41321981829d32
Stanislav Dashkov <stdashkov@mail.ru>
2017-11-28 15:54:14 +0300

    Объекты: передача по ссылке

--- js.js
+++ js.js
@@ -1,9 +1,144 @@
-"use strict";
+// "use strict";
+// ALL COSTS
+//
+// var salaries = {
+//   "Вася": 100,
+//   "Петя": 300,
+//   "Даша": 250
+// };
+//
+// //... ваш код выведет 650
+// var sum = 0;
+// for(var prop in salaries) {
+//   sum = sum + salaries[prop];
+// }
+// console.log(sum);
 
-var salaries = {
-  "Вася": 100,
-  "Петя": 300,
-  "Даша": 250
-};
+// "use strict";
+//
+// var salaries = {
+//   "Вася": 100,
+//   "Петя": 300,
+//   "Даша": 250
+// };
+//
+// // ... ваш код выведет "Петя"
+// var max = 0;
+// var maxName = "";
+// for (var prop in salaries) {
+//   if(max< salaries[prop]){
+//     max = salaries[prop];
+//     maxName = prop;
+//   }
+// }
+// alert( maxName || "нет сотрудников" );
 
-//... ваш код выведет 650
+// var menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu"
+// };
+//
+// function isNumeric(n) {
+//   return !isNaN(parseFloat(n)) && isFinite(n)
+// }
+//
+// function multiplyNumeric(obj){
+//   for(var prop in obj) {
+//     if(isNumeric(obj[prop])) {
+//       obj[prop] *=2;
+//     }
+//   }
+// }
+// multiplyNumeric(menu);
+// alert( "menu width=" + menu.width + " height=" + menu.height + " title=" + menu.title );
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
