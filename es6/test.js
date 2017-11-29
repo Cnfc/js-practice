56e7f87594c64237c8fdef6d97a03ea663cd90a9
Stanislav Dashkov <stdashkov@mail.ru>
2017-11-29 19:08:10 +0300

    chapter 2 tools js6

---
+++ es6/test.js
@@ -0,0 +1,16 @@
+'use strict';
+// left side
+const sentences = [
+  { subject: 'JavaScript', verb: 'is', object: 'great'},
+  { subject: 'Elephants', verb: 'are', object: 'large'},
+];
+
+//es6
+function say({ subject, verb, object}) {
+  console.log('${subject} ${verb} ${object}');
+}
+
+//for... of
+for(let s of sentences) {
+  say(s);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 const gulp   = require('gulp');
 const babel  = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
-  // Gulp
+  // ESLINT
+  gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+  .pipe(eslint())
+  .pipe(eslint.format());
+
+  // NODE FOR
   gulp.src("es6/**/*.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
-  // Browser for
-  gulp.scr("public/es6/**/*.js")
   .pipe(babel())
-  .pipe(gulp.dest("public/dest"));
+  .pipe(gulp.dest("dist"));
+
+  //BROWSER FOR
+  gulp.src("public/es6/**/*.js")
+  .pipe(babel())
+  .pipe(gulp.dest("public/dist"));
 });
