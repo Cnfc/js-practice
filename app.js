f193949d625301639246a0fe9348f0e81f2ad099
cnfc <stdashkov@mail.ru>
2017-12-20 21:32:52 +0300

    add income & expense constructor

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 //  ========== BUDGET CONTROLLER ============
 var budgetController = (function() {
 
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
 
 })();
 
-// budgetController.publicTest(5);
+
+
 
 // =========== UI CONTROLLER ==========================
 var UIController = (function() {
