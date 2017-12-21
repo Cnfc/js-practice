e6b546dc515ddf8cbf3c3e2301738c8bff9768e5
cnfc <stdashkov@mail.ru>
2017-12-21 13:57:56 +0300

    add income list

--- app.js
+++ app.js
@@ -69,6 +69,8 @@ var UIController = (function() {
     inputDescription: ".add__description",
     inputValue: ".add__value",
     inputBtn: ".add__btn",
+    incomeContainer: ".income__list",
+    expensesContainer: ".expenses__list",
   };
 
   return {
@@ -78,6 +80,28 @@ var UIController = (function() {
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+
+    addListItem: function(obj, type) {
+      var html, newHtml, element;
+      // Create html string
+      if(type === "inc") {
+        element = DOMstrings.incomeContainer;
+        html =  '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === "exp") {
+
+        element = DOMstrings.expensesContainer;
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description% rent</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+      // Replace placeholder
+      newHtml = html.replace("%id%", obj.id);
+      newHtml = newHtml.replace("%description%",obj.description);
+      newHtml = newHtml.replace("%value%",obj.value);
+
+      // Insert HTML into DOM
+      document.querySelector(element).insertAjacentHTML("beforeend", newhtml);
+
+
     },
 
     getDOMstrings: function() {
@@ -115,6 +139,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     //2 add item budget controller
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     //3 add item to ui
+    UICtrl.addListItem(newItem, input.type);
+
 
     //4 calc budget
 
