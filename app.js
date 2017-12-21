8a9bbc3f740ca9a6e780c331cabb9c59602b67e4
cnfc <stdashkov@mail.ru>
2017-12-21 12:27:03 +0300

    add nre item to our budget controller

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var budgetController = (function() {
   };
 
 
-
   var data = {
     allItems: {
       exp: [],
@@ -23,6 +22,36 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
+    }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      // CReate new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on "inc or exp" type
+      if(type === "exp") {
+        newItem =  new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem =  new Income(ID, des, val);
+
+      }
+
+      // push it into out datastructure
+      data.allItems[type].push(newItem);
+      // return new element
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data);
     }
   };
 
@@ -79,11 +108,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 
   var ctrlAddItem = function() {
+    var input, newItem;
     //1 get field input
-    var input = UICtrl.getInput();
-    console.log(input);
-    //2 add item budget controller
+    input = UICtrl.getInput();
 
+    //2 add item budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     //3 add item to ui
 
     //4 calc budget
