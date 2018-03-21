418f3144ba68b9223b4aded1cef593bafb17b6ac
cnfc <stdashkov@mail.ru>
2018-03-21 20:59:29 +0300

    add getUser async

--- git/github.js
+++ git/github.js
@@ -2,15 +2,21 @@ class Github {
   constructor(){
     this.client_id = '04f5fa2ea661af652da0';
     this.client_secret = 'dba41aef7c476f81cfeba0742c90644d95c56fa3';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
-} 
+}
