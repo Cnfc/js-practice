802428a6b7b41e782ecc24073c9f9f5249f0bc81
cnfc <stdashkov@mail.ru>
2018-03-26 20:35:40 +0300

    add profile next item

---
+++ profileScrollng/app.js
@@ -0,0 +1,83 @@
+
+const data = [
+  {
+    name: 'John Doe',
+    age: 32,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Boston MA',
+    image: 'https://randomuser.me/api/portraits/men/82.jpg'
+  },
+  {
+    name: 'Jen Smith',
+    age: 26,
+    gender: 'female',
+    lookingfor: 'female',
+    location: 'Boston MA',
+    image: 'https://randomuser.me/api/portraits/women/82.jpg'
+  },
+  {
+    name: 'Will ME',
+    age: 19,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Lynn MA',
+    image: 'https://randomuser.me/api/portraits/men/82.jpg'
+  },
+];
+
+const profiles = profileIterator(data);
+
+document.getElementById('next').addEventListener('click', nextProfile);
+
+function nextProfile() {
+    const currentProfile = profiles.next().value;
+
+    document.getElementById('profileDisplay').innerHTML = `
+    <ul class="list-group">
+      <li class="list-group-item">Name: ${currentProfile.name}
+      </li>
+      <li class="list-group-item">Age: ${currentProfile.age}
+      </li>
+      <li class="list-group-item">Location: ${currentProfile.location}
+      </li>
+      <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}
+      </li>
+          </ul>
+    `;
+
+    document.getElementById('imageDisplay').innerHTML =  `<img src="${currentProfile.image}">`;
+}
+
+function profileIterator(profiles){
+  let nextIndex = 0;
+
+  return {
+    next: function() {
+      return nextIndex < profiles.length ? { value: profiles[nextIndex++], done: false} : {done: true}
+    }
+  };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// a
