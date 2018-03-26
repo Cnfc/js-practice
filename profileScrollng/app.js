be32e3895caf8c12e51fd9c5de81cf3ba783bd28
cnfc <stdashkov@mail.ru>
2018-03-26 20:39:42 +0300

    complete profileScrolling

--- profileScrollng/app.js
+++ profileScrollng/app.js
@@ -22,31 +22,37 @@ const data = [
     gender: 'male',
     lookingfor: 'female',
     location: 'Lynn MA',
-    image: 'https://randomuser.me/api/portraits/men/82.jpg'
+    image: 'https://randomuser.me/api/portraits/men/43.jpg'
   },
 ];
 
 const profiles = profileIterator(data);
+nextProfile();//First profile
 
 document.getElementById('next').addEventListener('click', nextProfile);
 
 function nextProfile() {
     const currentProfile = profiles.next().value;
 
-    document.getElementById('profileDisplay').innerHTML = `
-    <ul class="list-group">
-      <li class="list-group-item">Name: ${currentProfile.name}
-      </li>
-      <li class="list-group-item">Age: ${currentProfile.age}
-      </li>
-      <li class="list-group-item">Location: ${currentProfile.location}
-      </li>
-      <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}
-      </li>
-          </ul>
-    `;
+if(currentProfile !== undefined) {
+  document.getElementById('profileDisplay').innerHTML = `
+  <ul class="list-group">
+  <li class="list-group-item">Name: ${currentProfile.name}
+  </li>
+  <li class="list-group-item">Age: ${currentProfile.age}
+  </li>
+  <li class="list-group-item">Location: ${currentProfile.location}
+  </li>
+  <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}
+  </li>
+  </ul>
+  `;
+  document.getElementById('imageDisplay').innerHTML =  `<img src="${currentProfile.image}">`;
 
-    document.getElementById('imageDisplay').innerHTML =  `<img src="${currentProfile.image}">`;
+} else {
+  window.location.reload();
+}
+
 }
 
 function profileIterator(profiles){
